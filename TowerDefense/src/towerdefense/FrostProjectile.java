2d8c4f739c19f99ead6930a2a7a8e9b155d86afb
m.dondorp <m.dondorp@2ce32150-b140-0410-8933-672d1753f5b6>
2007-12-17 14:34:25 +0000

    Added FrostTower & Exlode.png (does nothing)

---
+++ TowerDefense/src/towerdefense/FrostProjectile.java
@@ -0,0 +1,16 @@
+package towerdefense;
+
+public class FrostProjectile extends BaseProjectile
+{
+	public FrostProjectile(TowerDefense game, Mob pointer, BaseTower p)
+	{
+		super(game, pointer, p);
+		
+		setImage("/images/FrostProjectile.png", 5, 5);
+	}
+	
+	public void slowSpeed()
+	{
+		target.setSpeed(target.getSpeed()*.5);
+	}
+}
---
+++ TowerDefense/src/towerdefense/FrostTower.java
@@ -0,0 +1,59 @@
+package towerdefense;
+
+import phonegame.*;
+
+public class FrostTower extends BaseTower
+{
+	public FrostTower(TowerDefense game)
+	{
+		super(game);
+		
+		cashvalue = 200;
+		towerType = "Frost Tower";
+		
+		setImage("/images/FrostTower.png", 20, 20);
+	}
+	
+	protected void fire()
+	{
+		BaseProjectile projectile = new FrostProjectile(mygame, target, this);
+		projectile.setSpeed( 10 );
+		projectile.setPosition(this.getX() + (this.getFrameWidth() / 2) - (projectile.getFrameWidth() / 2), this.getY() + (this.getFrameHeight() / 2) - (projectile.getFrameHeight() / 2));
+		projectile.setDamage( damage );
+		
+		mygame.addGameItem(projectile);
+	}
+	
+	public void incFireRateLevel()
+	{
+		int cashreq = 0;
+		if( cashreq < mygame.getMoney() )
+		{
+			mygame.addMoney( - cashreq );
+			fireratelevel++;
+			firerate = (int) Math.floor( 35 - .15 * fireratelevel );
+		}
+	}
+	
+	public void incDistanceLevel()
+	{
+		int cashreq = 0;
+		if( cashreq < mygame.getMoney() )
+		{
+			mygame.addMoney( - cashreq );
+			distancelevel++;
+			maxdist = (double) ( 50 + .5 * distancelevel );
+		}
+	}
+	
+	public void incPowerLevel()
+	{
+		int cashreq = 0;
+		if( cashreq < mygame.getMoney() )
+		{
+			mygame.addMoney( - cashreq );
+			powerlevel++;
+			damage = (int) Math.floor( 25 + 10 * powerlevel );
+		}
+	}
+}
